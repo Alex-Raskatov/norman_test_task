00046822c699d326490a6426a62153bd3be31dc6
Alex-Raskatov <raskatov.study@gmail.com>
2023-02-19 18:58:19 +0300

    add class Vector

--- main.cpp
+++ main.cpp
@@ -5,126 +5,193 @@ const double sigma = 1, epsilon = 1, mass = 1;
 const double tau = std::sqrt(std::pow(sigma, 2)*mass/epsilon);
 const double t = tau/1000;
 
+class Vector {
+    
+    public:
+
+        double x;
+        double y;
+        double z;
+
+    Vector(double input_x, double input_y, double input_z) {
+        x = input_x;
+        y = input_y;
+        z = input_z;
+    }
+
+    double GetX() {
+        return x;
+    }
+
+    double GetY() {
+        return y;
+    }
+
+    double GetZ() {
+        return z;
+    }
+
+    void SetX(double input_x) {
+        x = input_x;
+    }
+
+    void SetY(double input_y) {
+        y = input_y;
+    }
+
+    void SetZ(double input_z) {
+        z = input_z;
+    }
+
+    void AddVector(Vector vec){
+        x += vec.x;
+        y += vec.y;
+        z += vec.z;
+    }
+
+    Vector AddVectorReturn (Vector vec){
+        Vector result(x + vec.GetX(), y + vec.GetY(), z + vec.GetZ());
+        return result;
+    }
+
+    void MultiOnConst (double k) {
+        x *= k;
+        y *= k;
+        z *= k;
+    }
+
+    Vector MultiOnConstReturn (double k) {
+        Vector result(x*k, y*k, z*k);
+        return result;
+    }
+};
+
 class Atom {
+
 
     private:
     //coordinats
-        double x_coor;
-        double y_coor;
-        double z_coor;
+        Vector coordinates;
 
     //speed
-        double x_speed;
-        double y_speed;
-        double z_speed;
+        Vector speed;
 
     //force
-        double x_force;
-        double y_force;
-        double z_force;
+        Vector force;
 
     public:
 
-        Atom(double x, double y, double z, double v_x, double v_y, double v_z){
+        Atom(double x, double y, double z, double v_x, double v_y, double v_z, double f_x = 0, double f_y = 0, double f_z = 0): coordinates(0,0,0), speed(0,0,0), force(0,0,0) {
+            
+            coordinates.SetX(x);
+            coordinates.SetY(y);
+            coordinates.SetZ(z);
+            
+            speed.SetX(v_x);
+            speed.SetY(v_y);
+            speed.SetZ(v_z);
 
-            x_coor = x;
-            y_coor = y;
-            z_coor = z;
+            force.SetX(f_x);
+            force.SetY(f_y);
+            force.SetZ(f_z);
 
-            x_speed = v_x;
-            y_speed = v_y;
-            z_speed = v_z;
         }
 
-        double get_x_coor() {
-            return x_coor;
+        double GetX() {
+            return coordinates.GetX();
         }
 
-        double get_y_coor() {
-            return y_coor;
+        double GetY() {
+            return coordinates.GetY();
         }
 
-        double get_z_coor() {
-            return z_coor;
+        double GetZ() {
+            return coordinates.GetZ();
         }
 
-        void set_x_coor(double x) {
-            x_coor = x;
+        void SetX(double x) {
+            coordinates.SetX(x);
         }
 
-        void set_y_coor(double y) {
-            y_coor = y;
+        void SetY(double y) {
+            coordinates.SetY(y);
         }
 
-        void set_z_coor(double z) {
-            z_coor = z;
+        void SetZ(double z) {
+            coordinates.SetZ(z);
         }
 
-        double get_x_speed() {
-            return x_speed;
+        double GetXSpeed() {
+            return speed.GetX();
         }
 
-        double get_y_speed() {
-            return y_speed;
+        double GetYSpeed() {
+            return speed.GetY();
         }
 
-        double get_z_speed() {
-            return z_speed;
+        double GetZSpeed() {
+            return speed.GetZ();
         }
 
-        void set_x_speed(double v_x) {
-            x_speed = v_x;
+        void SetXSpedd(double v_x) {
+            speed.SetX(v_x);
         }
 
-        void set_y_speed(double v_y) {
-            y_speed = v_y;
+        void SetYSpeed(double v_y) {
+            speed.SetY(v_y);
         }
 
-        void set_z_speed(double v_z) {
-            z_speed = v_z;
+        void SetZSpeed(double v_z) {
+            speed.SetZ(v_z);
         }
 
-        double get_x_force() {
-            return x_force;
+        double GetXForce() {
+            return force.GetX();
         }
 
-        double get_y_force() {
-            return y_force;
+        double GetYForce() {
+            return force.GetY();
         }
 
-        double get_z_force() {
-            return z_force;
+        double GetZForce() {
+            return force.GetZ();
         }
 
         void null_force() {
-            x_force = 0;
-            y_force = 0;
-            z_force = 0;
+            force.SetX(0);
+            force.SetY(0);
+            force.SetZ(0);
         }
 
         void calculate_force(Atom atom) {
 
-            double r = std::sqrt(x_coor*x_coor + y_coor*y_coor + z_coor*z_coor);
-            double norm_r_x = (atom.get_x_coor() - x_coor) / r;
-            double norm_r_y = (atom.get_y_coor() - y_coor) / r;
-            double norm_r_z = (atom.get_z_coor() - z_coor) / r;
+            double r = std::sqrt(coordinates.GetX()*coordinates.GetX()
+                                 + coordinates.GetY()*coordinates.GetY()
+                                 + coordinates.GetZ()*coordinates.GetZ());
+            double norm_r_x = (atom.GetX() - coordinates.GetX()) / r;
+            double norm_r_y = (atom.GetY() - coordinates.GetY()) / r;
+            double norm_r_z = (atom.GetZ() - coordinates.GetZ()) / r;
 
-            double force = -24*( std::pow((1/r),7) - 2*std::pow((1/r), 13));
+            double force_to_atom = -24*( std::pow((1/r),7) - 2*std::pow((1/r), 13));
 
-            x_force += force*norm_r_x;
-            y_force += force*norm_r_y;
-            z_force += force*norm_r_z;
+            Vector force_to_atom_vec(force_to_atom*norm_r_x,
+                            force_to_atom*norm_r_y,
+                            force_to_atom*norm_r_z);
+
+            force.AddVector(force_to_atom_vec);
         }
 
         void move() {
             
-            x_speed += x_force*t;
-            y_speed += y_force*t;
-            z_speed += z_force*t;
-
-            x_coor += x_speed*t;
-            y_coor += y_speed*t;
-            z_coor += z_speed*t;
+            speed.AddVector(force.MultiOnConstReturn(t));
+            coordinates.AddVector(speed.MultiOnConstReturn(t));
         }
 };
 
+int CreateDrop (double drops_radius, Vector drops_center, double lattice_step, double atoms_max_speed_proection, Vector drops_speed) {
+    return 0;
+}
+
+int main () {
+    return 0;
+}
